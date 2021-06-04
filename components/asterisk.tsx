cd1500885d35767c7ca2b55c4b8384cbac22fa7f
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2021-06-04 00:02:36 -0700

    wip
    TODO
    
    - animate the different stages
    - https://www.figma.com/file/CLDXLlQLdZiIeB8p5YHPCT/stream.new?node-id=1701%3A464

--- components/asterisk.tsx
+++ components/asterisk.tsx
@@ -2,9 +2,26 @@ interface Props {
   size?: number;
 }
 
-const Asterisk: React.FC<Props> = ({ size = 46 }) => (
-  <svg width={size} height={size} viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M28.1765 3.68164L17.8238 42.3187M3.68164 17.8238L42.3187 28.1765M17.8185 3.70145L28.1818 42.2989M8.87783 8.86333L37.1225 37.137M3.70145 28.1818L42.2989 17.8185M8.86333 37.1225L37.137 8.87783" stroke="#f8f8f8" strokeWidth="3" strokeMiterlimit="10" />
+const Asterisk: React.FC<Props> = (props) => (
+  <svg
+    width={20}
+    height={32}
+    viewBox="0 0 20 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g
+      style={{
+        mixBlendMode: "exclusion",
+      }}
+      fill="#fff"
+    >
+      <circle cx={4} cy={28} r={4} />
+      <circle cx={4} cy={4} r={4} />
+      <circle cx={16} cy={16} r={4} />
+      <circle cx={4} cy={16} r={4} />
+    </g>
   </svg>
 );
 
